fc59be3bb7c2bab7bc8497c65fbe74b126c4b984
brotherc <1157730239@qq.com>
2025-04-23 22:49:01 +0800

    feat: 解决跨域

--- src/main/java/com/brotherc/documentcenter/config/WebConfig.java
+++ src/main/java/com/brotherc/documentcenter/config/WebConfig.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -42,6 +45,21 @@ public class WebConfig {
             long timestamp = parser.getValueAsLong();
             return timestamp < 0 ? null : LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
         }
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 支持通配符
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 
 }
